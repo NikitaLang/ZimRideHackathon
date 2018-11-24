94e19078079d7a8d4dd22670e1ff16e91da3ac48
Nikita Lang <nikitalang@nikitas-imac.local>
2018-11-24 13:06:36 -0800

    Initial Commit

---
+++ ZimRideMobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZimRideMobile
+//
+//  Created by Nikita Lang on 11/24/18.
+//  Copyright © 2018 Nikita Lang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
